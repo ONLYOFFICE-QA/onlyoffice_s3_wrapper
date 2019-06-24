9733d6421b5bfd143c6d66a4b30865b569c734b4
Pavel Lobashov <shockwavenn@gmail.com>
2019-06-24 15:34:32 +0300

    Fix RuboCop `Layout/SpaceInsideBlockBraces` cop

--- spec/download_spec.rb
+++ spec/download_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe OnlyofficeS3Wrapper do
 
   it 'download_object for nonexitsing name cause exception' do
     fake_name = 'docx/fake-name.fakeext'
-    expect { s3.download_file_by_name(fake_name)}.to raise_error(/not found/)
+    expect { s3.download_file_by_name(fake_name) }.to raise_error(/not found/)
   end
 
   after :each do
