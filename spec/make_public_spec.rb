7263a5a83e3e253e7caf25c334dbae947dba419a
Pavel Lobashov <shockwavenn@gmail.com>
2020-01-23 19:21:55 +0300

    Reduce `Metrics/BlockLength` cop

---
+++ spec/make_public_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe OnlyofficeS3Wrapper do
+  let(:make_public_result) do
+    file_name = "#{SecureRandom.uuid}.ext"
+    OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: "/tmp/#{file_name}",
+                                                              content: '')
+    s3.upload_file("/tmp/#{file_name}", 'test')
+    s3.make_public("test/#{file_name}")
+  end
+
+  it 'make_public result a link' do
+    expect(make_public_result[0]).to be_a(String)
+  end
+
+  it 'make_public permissions is READ' do
+    expect(make_public_result[1]).to eq('READ')
+  end
+end
