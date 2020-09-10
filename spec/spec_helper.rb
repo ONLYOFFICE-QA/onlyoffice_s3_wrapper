be0e5dc490486ced6fbc8a48705d2df9c69348f1
Pavel Lobashov <shockwavenn@gmail.com>
2020-09-10 19:13:07 +0300

    Fix require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'simplecov'
 SimpleCov.start
 
-require 'bundler/setup'
 require 'onlyoffice_s3_wrapper'
 require 'open-uri'
 
